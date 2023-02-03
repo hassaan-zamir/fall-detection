025d6185d0b2c1f2797c7962bd9d2438ef49c867
hassaan-zamir <hassaanzamir94@gmail.com>
2023-02-03 22:09:43 +0500

    new

--- main.py
+++ main.py
@@ -176,7 +176,7 @@ if __name__ == '__main__':
         if outvid:
             writer.write(frame)
 
-        cv2.imshow('frame', frame)
+        # cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
